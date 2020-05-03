267a3a5068d85c7ac2f9e3d31fda3a54368947fe
tnasergey3 <tnasergey@gmail.com>
2020-05-03 03:00:46 +0300

    2020.05.03

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -66,6 +66,11 @@ namespace WebApp
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapAreaControllerRoute(
+                    name: "admin_area",
+                    areaName: "admin",
+                    pattern: "admin/{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
